3a574311595352dc63b1e9860617fa7a1a16065c
AnhMinh <minhnase173546@fpt.edu.vn>
2024-11-18 00:26:43 +0700

    push

--- backend-api/src/controller/homeController.js
+++ backend-api/src/controller/homeController.js
@@ -1,4 +1,12 @@
+ const conn = require('../config/database.js')
 const getHomepage = (req , res) => {
+    //call model 
+    conn.query(
+        'select * from Users u ',
+        function (err , results, feilds) { 
+            console.log('result >>> ',results);
+        }
+      );
     res.send('Hello woeld');
 }
 const getAnhMinh = (req , res ) => {
--- backend-api/src/server.js
+++ backend-api/src/server.js
@@ -6,24 +6,12 @@ const app = express()
 const port =process.env.PORT ;
 const hostname = process.env.HOSTNAME
 const webRoutes = require('./routes/web.js')
-const mysql = require('mysql2')
+
 /* **************************************************************** */
 //test connecttion 
-const conn = mysql.createConnection({
-    host: 'localhost',
-    port: 3308,
-    user: 'root',
-    password:'123456',
-    database: 'anhminh',
-  });
+
   //query 
-  conn.query(
-    'select * from Users u ',
-    function (err , results, feilds) { 
-        console.log('result >>> ',results);
-        console.log('feilds >>> ' , feilds);
-    }
-  )
+
 /* config engine  */
 configViewEngine(app);
 /* Routes   */
